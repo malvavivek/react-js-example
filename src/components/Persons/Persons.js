61c7c638dca17716461d8505631955dc6c300241
Vivek Malva <vivmalva@publicisgroupe.net>
2019-03-11 19:18:33 +0530

    day4-1

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,10 +1,16 @@
 import React from 'react';
-// import Person from "./Person/Person"
+import Person from "./Person/Person"
 const persons = (props)=>{
     props.PersonArr.map((person,index)=>{
         return (
-        <div>
-    <p>gfrhtr</p></div>)
+        <div><Person 
+        click = {()=>props.clicked(index)}
+        name={person.name}
+        age={person.age}
+        key={person.id}
+        changed={(event)=>props.changed(event,person.id)}
+        />
+        </div>)
       })
 };
 
